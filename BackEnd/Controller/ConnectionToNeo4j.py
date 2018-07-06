76d84bc0749586e780013fa12bc136eea0b8b584
Senuri12 <senuseneviratneit@gmail.com>
2018-07-06 12:42:50 +0530

    commit update sarindi

--- BackEnd/Controller/ConnectionToNeo4j.py
+++ BackEnd/Controller/ConnectionToNeo4j.py
@@ -21,5 +21,48 @@ def getValueFromdb(subsection):
     print(validationValue)
     return validationValue
 
+def cvQuestionGen(id):
+  query = "MATCH (j:CV{id:"+ id+"}) RETURN j.topic"
+  gen_Question = graph.run(query).evaluate()
+  # print(gen_Question)
+  return gen_Question
+
+
+
+
+def session_Node_Count(session):
+  # session ="2"
+  query = "MATCH (a:CV{session: " +session+ "}) RETURN count(*)"
+  gen_count = graph.run(query).evaluate()
+  # print(gen_count)
+  return  gen_count
+
+
+def get_node_id(session):
+  query = "MATCH (a:CV{session: " + session + "}) RETURN a.id"
+  gen_count = graph.run(query).evaluate()
+  # print(gen_count)
+  return gen_count
+# get_node_id("3")
+
+
+
+def technical_question_keyword(table,id):
+  val = "1"
+
+
+  query = "MATCH(a:language{Name:'"+table+"'}) - [r: has]->(b:sub{id:'"+id+"'})RETURN b.Name"
+  gen_Question = graph.run(query).evaluate()
+  # print(gen_Question)
+  return gen_Question
+# table ="java"
+# technical_question_keyword(table)
+
+def getTechNodeCount(db):
+  query = "MATCH(a:language{Name:'"+db+"'}) - [r: has]->(b:sub{})RETURN count(*)"
+  gen_count = graph.run(query).evaluate()
+  # print(gen_count)
+  return gen_count
+
 
 
---
+++ BackEnd/Database/NonTechnicalQuestionDirectory.py
@@ -0,0 +1,9 @@
+l1 = ['NNP','JJ','NNS']
+l2 = ["NN","PRP"]
+l3 = ["PRP$","NNS"]
+l4 = ["PRP$","JJ","NN"]
+l5 = ["JJ","NNS"]
+
+
+
+
