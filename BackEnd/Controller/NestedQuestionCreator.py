9202296018cf507632824b47cb6816a5fc3cf2fc
Senuri12 <senuseneviratneit@gmail.com>
2018-08-24 16:00:10 +0530

    sarindi commit

--- BackEnd/Controller/NestedQuestionCreator.py
+++ BackEnd/Controller/NestedQuestionCreator.py
@@ -1,23 +1,27 @@
-import ConnectionToNeo4j
-filtered_words_string = "pickling  is  a good python concept"
-filtered_words = filtered_words_string.split(" ")
-db ="python"
-topic_list = []
+import ConnectionToNeo4j,random
 
-# print(filtered_words)
-unique_filtered_words = set()
-for val in filtered_words:
-    lower_case_val = val.lower()
-    unique_filtered_words.add(lower_case_val)
-# print(unique_filtered_words)
+def keywordSelector():
+    filtered_words_string = "pickling  is  a unpickling good python concept"
+    filtered_words = filtered_words_string.split(" ")
+    db ="python"
+    topic_list = []
 
-for value in unique_filtered_words:
-    topic_availability = ConnectionToNeo4j.getMatchingTopics(db,value)
-    print(topic_availability)
-    if topic_availability == True:
-        topic_list.append(value)
-print(topic_list)
+    # print(filtered_words)
+    unique_filtered_words = set()
+    for val in filtered_words:
+        lower_case_val = val.lower()
+        unique_filtered_words.add(lower_case_val)
+    # print(unique_filtered_words)
 
+    for value in unique_filtered_words:
+        topic_availability = ConnectionToNeo4j.getMatchingTopics(db,value)
+        print(topic_availability)
+        if topic_availability == True:
+            topic_list.append(value)
+    print(topic_list)
 
+    random_keyword = random.choice(topic_list)
+    return  random_keyword
 
+keyword = keywordSelector();
 # for()
