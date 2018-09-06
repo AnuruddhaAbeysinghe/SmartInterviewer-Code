6ef9c5f79171ba3163d7be3946ae05a9bc051eea
Senuri12 <senuseneviratneit@gmail.com>
2018-09-06 23:22:48 +0530

    sarindo

---
+++ BackEnd/Controller/test.py
@@ -0,0 +1,7 @@
+nested_question_ccount = 2
+split_list_length = 3
+itteration_value = 3
+rem_nested_count = 11-(split_list_length * itteration_value)
+nested_question_ccount = nested_question_ccount + rem_nested_count
+print(rem_nested_count)
+print(nested_question_ccount)
