f4d4d0a44a8ea0dbb894d215bc31c0522f865bb6
Wick94 <ueetest8@gmail.com>
2018-09-10 20:52:55 +0530

    change_datamani

--- BackEnd/Controller/DataManipulate.py
+++ BackEnd/Controller/DataManipulate.py
@@ -7,6 +7,7 @@ graph = Graph()
 
 # vikum = Node("User", Id="U0002", name="nidarshana Vikram",age="24", School="studied School", AL="mathematics", university="Sri Lanka Institute Information Technology", weaknesses="laziness", Strengths="creativity", qualification="software engineering", project="Hotel Management System", technology="Java", GPA="3.5")
 # graph.create(vikum)
-# graph.delete_all()
 
-print(ConnectionToNeo4j.sessionMarksStoring('UID001','1','question2','0.8'))
+graph.delete_all()
+
+# print(ConnectionToNeo4j.sessionMarksStoring('UID001','1','question2','0.8'))
