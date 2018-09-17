8d73da288fe83ed97130753cc3063dc1a6847c4f
Wick94 <ueetest8@gmail.com>
2018-09-17 15:53:22 +0530

    17/09/18_completed_vikz

--- BackEnd/Controller/Validation.py
+++ BackEnd/Controller/Validation.py
@@ -6,6 +6,6 @@ q="java  overriding object  technical"
 # subsection = input("enter sub area : \n")
 # typer = input("enter type : \n")pickling
 value = []
-value = SpeachToText.validation("pickling", "technical","nonested","question3")
+value = SpeachToText.validation("pickling", "technical","nonested","question5")
 print(value)
 
