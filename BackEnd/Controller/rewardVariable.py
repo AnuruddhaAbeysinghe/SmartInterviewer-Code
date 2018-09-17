e8bcc5433c4236cbd8c539fc05cc66bc25194065
unknown <chamisinha@gmail.com>
2018-09-17 18:38:46 +0530

    create a function for working my file

--- BackEnd/Controller/rewardVariable.py
+++ BackEnd/Controller/rewardVariable.py
@@ -1,3 +1,19 @@
+#-----For check whether function is working
+
+# from BackEnd.Controller import CreateReward
+#
+# facial = 15
+# voice = 15
+# answer = 20
+#
+# userid = "uid001"
+# langName = "python"
+# nodeid = "6"
+# category = "easy"
+#
+# CreateReward.rewardForQuestion(facial,voice,answer,userid,langName,nodeid,category)
+
+#----For Integrate with sarindi--------------------------
 
 def getLanguageName(getLang):
     global langName
